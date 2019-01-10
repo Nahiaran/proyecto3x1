95bd40c000fc4db21560da56edb43ba93c677897
AS3-2-06 <AS3-2-06@uni.lan>
2019-01-10 13:56:50 +0100

    1

--- registrar.php
+++ registrar.php
@@ -2,15 +2,17 @@
 $usuario=$_POST["usuario"];
 $contraseña=$_POST["pass_usuario"];
 $hash=password_hash($contraseña, PASSWORD_DEFAULT);
+
 $servername="localhost:3307";
 $username="root";
 $password="";
 $dbname="proyecto3x1";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$insert = "INSERT INTO usuarios(usuario, contraseña) VALUES ('$usuario','$hash')";
+$insert = "INSERT INTO usuarios(usuario, pass_usuario) VALUES ('$usuario','$hash')";
 //echo $insert;
 $query = $conn -> query("$insert");
   

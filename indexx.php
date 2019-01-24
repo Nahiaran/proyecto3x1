24964774ca5635d70df9338e5fff588f0d23f13d
AS3-2-06 <AS3-2-06@uni.lan>
2019-01-24 13:24:52 +0100

    a

--- indexx.php
+++ indexx.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-if (isset($_SESSION['usuario'])) {
-  echo "Sesion inicializada";
+if (!isset($_SESSION['usuario'])) {
+    header('Location: '."login.html");
 }
 else {
     require_once 'vendor/autoload.php';
     $loader = new Twig_Loader_Filesystem('templates');
     $twig = new Twig_Enviroment($loader);
-    
+
 }
 ?>

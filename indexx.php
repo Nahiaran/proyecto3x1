deb0f55fd6d67dc47b32a29afbfcc2e66b33108a
AS3-2-06 <AS3-2-06@uni.lan>
2019-01-24 13:39:29 +0100

    a

--- indexx.php
+++ indexx.php
@@ -3,10 +3,13 @@ session_start();
 if (!isset($_SESSION['usuario'])) {
     header('Location: '."login.html");
 }
-else {
+/*else {
     require_once 'vendor/autoload.php';
     $loader = new Twig_Loader_Filesystem('templates');
     $twig = new Twig_Enviroment($loader);
 
-}
+}*/
+?>
+<?php
+echo "Bienvenido ".$_SESSION["usuario"];
 ?>

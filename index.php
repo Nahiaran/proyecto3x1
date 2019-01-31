3a0a117eac2475a90fa5cd4d1bc2d5e66334eba1
AS3-2-06 <AS3-2-06@uni.lan>
2019-01-31 13:17:01 +0100

    a

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['usuario'])) {
 }*/
 ?>
 <html>
-<title>W3.CSS Template</title>
+<title>Proyecto 3x1</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">

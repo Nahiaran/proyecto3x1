81d631454a5e487ac32fd1cd78813115e6ec434a
Nahiaran <32323107+Nahiaran@users.noreply.github.com>
2019-02-01 11:53:38 +0100

    GráficaFINAL

---
+++ chart/data.php
@@ -0,0 +1,25 @@
+<?php
+
+$dbhost = 'localhost:3307';
+$dbname = 'proyecto3x1';
+$dbuser = 'root';
+$dbpass = '';
+
+try{
+
+    $dbcon = new PDO("mysql:host={$dbhost};dbname={$dbname}",$dbuser,$dbpass);
+    $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $ex){
+    die($ex->getMesage());
+}
+
+$stmt = $dbcon->prepare("SELECT nombre,visitantes FROM ciudades");
+$stmt->execute();
+$row1 = [];
+while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+    {
+        extract($row);
+        $row1[]= ['nombre' => $nombre, 'visitantes' => $visitantes];
+    }
+    echo json_encode($row1);
+?>

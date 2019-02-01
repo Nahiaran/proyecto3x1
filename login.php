ea2cfe43a285a4db4c58aabbc44e0ea156e494f4
AS3-2-06 <AS3-2-06@uni.lan>
2019-02-01 16:32:01 +0100

    a

--- login.php
+++ login.php
@@ -1,5 +1,16 @@
 <?php
-session_start(); // Iniciando sesion
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    header('Location: '."login.html");
+}
+/*else {
+    require_once 'vendor/autoload.php';
+    $loader = new Twig_Loader_Filesystem('templates');
+    $twig = new Twig_Enviroment($loader);
+
+}*/
+
+// Iniciando sesion
 // Define $username y $password
 $usuario=$_POST['usuario'];
 $pass_usuario=$_POST['pass_usuario'];

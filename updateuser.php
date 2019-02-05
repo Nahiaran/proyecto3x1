1bee0e09323e2e83dc1485b9edd4674b54d456ad
AS3-2-06 <AS3-2-06@uni.lan>
2019-02-05 09:21:53 +0100

    a

---
+++ updateuser.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if(!isset($_SESSION['usuario'])){
+	header('location: logindata.php');
+	}
+$error=''; // Variable para almacenar el mensaje de error
+if (isset($_POST['registrar'])) {
+if (empty($_POST['usuario']) || empty($_POST['pass_usuario']) || empty($_POST['email'])) {
+$error = "Debes rellenar todos los campos";
+}
+else
+{
+
+$usuario=$_POST["usuario"];
+$pass_usuario=$_POST["pass_usuario"];
+$hash=password_hash($pass_usuario, PASSWORD_DEFAULT);
+$email=$_POST["email"];
+
+include ("conexion.php");
+$selectuser = "SELECT * FROM usuarios WHERE usuario='$usuario'";
+$result = mysqli_query($conn, $selectuser);
+$count = mysqli_num_rows($result);
+
+if ($count == 0) {
+	$insert = "INSERT INTO usuarios(usuario, pass_usuario, email) VALUES ('$usuario','$hash', '$email')";
+	//echo $insert;
+	$query = $conn -> query("$insert");
+}
+else {
+	$error = "El usuario ya existe";
+}
+}
+}
+?>
---
+++ userinfo.php
@@ -0,0 +1,44 @@
+<?php 
+//include ("login.php"); 
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Proyecto 3x1 - Mi Perfil</title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<!-- for-mobile-apps -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="keywords" content="Flat Login Form Widget Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<!-- //for-mobile-apps -->
+<!--Google Fonts-->
+<link href='//fonts.googleapis.com/css?family=Signika:400,600' rel='stylesheet' type='text/css'>
+<!--google fonts-->
+</head>
+<body>
+
+
+<h1>Mi Perfil</h1>
+<div class="header agile">
+	<div class="wrap">
+		<div class="login-main wthree">
+			<div class="login">
+			<h3>Iniciar sesión</h3>
+			<form action="#" method="post">
+				<input type="text" placeholder="Usuario" required name="usuario">
+				<input type="password" placeholder="Contraseña" name="pass_usuario">
+        <input name="submit" type="submit" value="Ingresar"><br><br>
+		<div class="clear"> </div>
+				<span><?php echo $error; ?></span>
+        <br><br>No est&aacutes registrado?
+        <a href="registrardata.php" class="text-center">Regístrate</a>
+		</div>
+			</form>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>

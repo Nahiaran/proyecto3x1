fcd076b08163babe1f8accd80e5b985cc71d6ac6
AS3-2-06 <AS3-2-06@uni.lan>
2019-02-08 16:43:28 +0100

    a

--- contacto.php
+++ contacto.php
@@ -33,7 +33,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
           ?>
       </span><br>
       <a href="index.php" class="w3-bar-item w3-button"><i class="fa fa-home"></i></a>
-      <a href="#" class="w3-bar-item w3-button"><i class="fa fa-user"></i></a>
+      <a href="userinfo.php" class="w3-bar-item w3-button"><i class="fa fa-user"></i></a>
       <a href="logout.php" class="w3-bar-item w3-button"><i class="fa fa-power-off"></i></a>
     </div>
 </div>
--- logindata.php
+++ logindata.php
@@ -20,26 +20,24 @@ include ("login.php");
 </head>
 <body>
 
-
 <h1>Formulario de inicio de sesión</h1>
 <div class="header agile">
-	<div class="wrap">
-		<div class="login-main wthree">
-			<div class="login">
-			<h3>Iniciar sesión</h3>
-			<form action="#" method="post">
+		<div class="wrap">
+				<div class="login-main wthree">
+						<div class="login">
+						<h3>Iniciar sesión</h3>
+						<form action="#" method="post">
 				<input type="text" placeholder="Usuario" required name="usuario">
 				<input type="password" placeholder="Contraseña" name="pass_usuario">
-        <input name="submit" type="submit" value="Ingresar"><br><br>
-		<div class="clear"> </div>
-				<span><?php echo $error; ?></span>
-        <br><br>No est&aacutes registrado?
-        <a href="registrardata.php" class="text-center">Regístrate</a>
+				<input name="submit" type="submit" value="Ingresar"><br><br>
+						<div class="clear"> </div>
+						<span><?php echo $error; ?></span>
+						<br><br>No est&aacutes registrado?
+						<a href="registrardata.php" class="text-center">Regístrate</a>
+						</div>
+						</form>
+				</div>
 		</div>
-			</form>
-			</div>
-		</div>
-	</div>
 </div>
 </body>
 </html>
